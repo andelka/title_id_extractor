b7b887d6bcca78d96e7a966eb739a3de16d241a0
andelka <a.tesarova@gmail.com>
2017-08-22 16:39:06 +0200

    adding condition for no results

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -6,14 +6,16 @@ class Keyword < ApplicationRecord
       csv << ["item_id", "item_title", "category_id", "category_name"]
       keywords = self.all
       keywords.each do |keyword|
-        items = keyword.get_items
-        parsed_items = JSON.parse(items.to_json)
-        items.each do |item|
-          item_id = item["itemId"]
-          item_title = item["title"]
-          category_id = item["primaryCategory"]["categoryId"]
-          category_name = item["primaryCategory"]["categoryName"]
-          csv << [item_id, item_title, category_id, category_name]
+        search_results = keyword.get_search_results
+        parsed_search_results = JSON.parse(search_results.to_json)
+        if items = search_results["item"]
+          items.each do |item|
+            item_id = item["itemId"]
+            item_title = item["title"]
+            category_id = item["primaryCategory"]["categoryId"]
+            category_name = item["primaryCategory"]["categoryName"]
+            csv << [item_id, item_title, category_id, category_name]
+          end
         end
       end
     end
@@ -23,8 +25,8 @@ class Keyword < ApplicationRecord
     @rebay ||= Rebay::Finding.new
   end
 
-  def get_items
-    rebay.find_items_by_keywords({keywords: text}).response["searchResult"]["item"]
+  def get_search_results
+    rebay.find_items_by_keywords({keywords: text}).response["searchResult"]
   end
 
 end

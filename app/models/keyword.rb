57305ae021abd419a5e320954b8cbb1f342c90fe
andelka <a.tesarova@gmail.com>
2018-01-11 17:23:12 +0100

    refactoring

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,35 +1,6 @@
 class Keyword < ApplicationRecord
   validates :text, presence: true
 
-  # def self.to_csv
-  #   CSV.generate do |csv|
-  #     csv << ["item_id", "item_title", "category_id", "category_name"]
-  #     keywords = self.all
-  #     keywords.each do |keyword|
-  #       search_results = keyword.get_search_results
-  #       result_count = keyword.get_result_count
-  #       #parsed_search_results = JSON.parse(search_results.to_json)
-  #       if result_count > 1
-  #         items = search_results["item"]
-  #         items.each do |item|
-  #           item_id = item["itemId"]
-  #           item_title = item["title"]
-  #           category_id = item["primaryCategory"]["categoryId"]
-  #           category_name = item["primaryCategory"]["categoryName"]
-  #           csv << [item_id, item_title, category_id, category_name]
-  #         end
-  #       elsif result_count == 1
-  #         items = search_results["item"]
-  #         item_id = items["itemId"]
-  #         item_title = items["title"]
-  #         category_id = items["primaryCategory"]["categoryId"]
-  #         category_name = items["primaryCategory"]["categoryName"]
-  #         csv << [item_id, item_title, category_id, category_name]
-  #       end
-  #     end
-  #   end
-  # end
-
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
       Keyword.create!(row.to_hash)
@@ -48,28 +19,24 @@ class Keyword < ApplicationRecord
     self.search_results["@count"].to_i
   end
 
+  def item
+    self.search_results["item"]
+  end
+
   def item_id
-      self.search_results["item"]["itemId"]
+    self.item["itemId"]
   end
 
   def item_title
-      self.search_results["item"]["title"]
+      self.item["title"]
   end
 
   def cat_id
-    #if self.result_count > 0
-      self.search_results["item"]["primaryCategory"]["categoryId"]
-    #end
+      self.item["primaryCategory"]["categoryId"]
   end
 
   def rebay_shop
     @rs ||= Rebay::Shopping.new
-  end
-
-  def cat_info
-    if self.cat_id
-      rebay_shop.get_category_info({CategoryID: self.cat_id}).response["CategoryArray"]["Category"]["CategoryIDPath"]
-    end
   end
 
 end
